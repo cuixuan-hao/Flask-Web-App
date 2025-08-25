c3fa37d873a5e15f1c8acd8ec317c2fcf8c231d7
simon <xuanhaocui@163.com>
2025-08-25 17:24:09 +0800

    Update models.py

--- website/models.py
+++ website/models.py
@@ -1,14 +1,6 @@
-from . import db
+rom . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
-
-
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,3 +8,9 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(1000))  # 减少到1000字符，练手够用
+    date = db.Column(db.DateTime, default=func.now())  # 简化，去掉timezone
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
