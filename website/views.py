3ace8d5b3c43928356f0e53c5082b6f0d80cbce1
simon <xuanhaocui@163.com>
2025-08-25 17:26:10 +0800

    Update views.py

--- website/views.py
+++ website/views.py
@@ -6,32 +6,30 @@ import json
 
 views = Blueprint('views', __name__)
 
-
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
     if request.method == 'POST': 
-        note = request.form.get('note')#Gets the note from the HTML 
-
-        if len(note) < 1:
-            flash('Note is too short!', category='error') 
-        else:
-            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-            db.session.add(new_note) #adding the note to the database 
+        note_content = request.form.get('note')
+        
+        if note_content:  # 简化验证：只要有内容就行
+            new_note = Note(data=note_content, user_id=current_user.id)
+            db.session.add(new_note)
             db.session.commit()
-            flash('Note added!', category='success')
-
+            flash('笔记已添加')
+    
     return render_template("home.html", user=current_user)
 
-
 @views.route('/delete-note', methods=['POST'])
+@login_required  # 添加登录验证
 def delete_note():  
-    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
-
+    note_data = json.loads(request.data)
+    note_id = note_data['noteId']
+    note = Note.query.get(note_id)
+    
+    # 简化：只检查笔记存在且属于当前用户
+    if note and note.user_id == current_user.id:
+        db.session.delete(note)
+        db.session.commit()
+    
     return jsonify({})
