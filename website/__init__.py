378b31cf2542f82984c01a5f69d424e792e8cca0
simon <xuanhaocui@163.com>
2025-08-26 09:40:04 +0800

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -10,15 +10,17 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    
     db.init_app(app)
     
     from .views import views
     from .auth import auth
-    from .models import User, Note
-    
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+    
+    from .models import User, Note
+    
+    with app.app_context():
+        db.create_all()
     
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
@@ -27,8 +29,5 @@ def create_app():
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
-    
-    with app.app_context():
-        db.create_all()
     
     return app
